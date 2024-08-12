e03dd12bdf5f636a4b1ab4a59cfe934b12f5f1d1
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-12 22:09:55 +0300

    Fixed look_at_from_position: parallel vectors UP and DOWN

--- Player.gd
+++ Player.gd
@@ -9,8 +9,8 @@ export var gravity = 200
 var velocity = Vector3.ZERO
 var direction = Vector3.ZERO
 
-func _ready():
-	direction.z = 0.1
+#func _ready():
+#	direction.z = 0.1
 
 func _physics_process(delta):
 	
@@ -29,8 +29,14 @@ func _physics_process(delta):
 	direction_to = direction_to.normalized()
 	direction = lerp(direction, direction_to, delta)
 	
-	# @TODO look_at_from_position: Up vector and direction...
-	$Pivot.look_at(translation + direction, Vector3.UP)
+	# Fix look_at_from_position: parallel vectors UP and DOWN
+	var pg = $Pivot.global_transform.origin
+	var pd = translation + direction
+	if pg[0] == pd[0] and pg[2] == pd[2]:
+		$Pivot.look_at(Vector3.FORWARD, Vector3.UP)
+	else: 
+		$Pivot.look_at(pd, Vector3.UP)
+		
 	# Fix Nose down glitch caused by Pivot.Translation.y > 0
 	$Pivot.rotation[0] = clamp($Pivot.rotation[0]+0.25, -0.5, +0.5)
 

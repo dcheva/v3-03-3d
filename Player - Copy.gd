19a4111ce5a22fde8fc227bfa0edd10534781542
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-12 18:29:28 +0300

    Testing Main.Player.y 0.25 and Player.Pivot.y Player.Collision.y 25

--- Player - Copy.gd
+++ Player - Copy.gd
@@ -4,17 +4,17 @@ extends KinematicBody
 export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 export var fall_acceleration = 75
-# LERP delta
-export var d = 4
 
 var velocity = Vector3.ZERO
 var direction = Vector3.ZERO
 
+func _ready():
+	pass
+
 func _physics_process(delta):
-	
-	var velocity_to = Vector3.ZERO
+	# Using LERP vector 0
 	var direction_to = Vector3.ZERO
-	
+	var velocity_to = Vector3.ZERO
 	if Input.is_action_pressed("move_right"):
 		direction_to.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -23,16 +23,22 @@ func _physics_process(delta):
 		direction_to.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction_to.z -= 1
-
-	direction_to = direction_to.normalized()
-	direction = lerp(direction, direction_to, delta*d)
 	
 	if direction_to != Vector3.ZERO:
-		$Pivot.look_at(translation + direction, Vector3.UP)
+		# Direction
+		direction_to = direction_to.normalized()
+		# Ground velocity
+		velocity_to.x = direction_to.x * speed
+		velocity_to.z = direction_to.z * speed
+		# Vertical velocity
+		velocity_to.y -= fall_acceleration * delta
 
-	velocity_to.x = direction.x * speed
-	velocity_to.z = direction.z * speed
-	velocity_to.y -= fall_acceleration * delta
-	velocity = lerp(velocity, velocity_to, delta*d)
+	# Applying LERP to vector 1
+	#direction = lerp(direction, direction_to, delta)
+	#velocity = lerp(velocity, velocity_to, delta)
+	direction = direction_to
+	velocity = velocity_to
 	
+	# Moving the character
+	$Pivot.look_at(translation + direction, Vector3.UP)
 	velocity = move_and_slide(velocity, Vector3.UP)
